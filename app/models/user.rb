510ef8d74f4cc4d3befc540ef7ecaedf179e8fed
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-10-10 10:36:58 +0000

    [Add]会員登録時の項目

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum age: { ten: 0, twenty: 1, thirty: 2, fourty: 3, fifty: 4, sixty: 5 }
+  enum gender: { male: 0, female: 1, other: 2 }
+  enum area: { secret: 0, hokkaido: 1, tohoku: 2, kanto: 3, chubu: 4, kinki: 5, chugokushikoku: 6, kyushu: 7, foreign: 8 }
+
 end
