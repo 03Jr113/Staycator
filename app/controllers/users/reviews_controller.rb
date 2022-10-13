a5ec29d408a79d6acdb63918e17dee7a41ccbe3f
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-10-13 05:20:42 +0000

    [Add]レビュー投稿・一覧・詳細・編集・削除機能（一部未実装）

---
+++ app/controllers/users/reviews_controller.rb
@@ -0,0 +1,57 @@
+class Users::ReviewsController < ApplicationController
+
+  before_action :correct_user, only: [:edit, :update]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to review_path(@review.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to review_path(@review.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:user_id, :date, :traveler, :title, :body)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to reviews_path unless @user == current_user
+  end
+
+end
