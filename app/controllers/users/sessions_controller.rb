c26ca1183e2029c2a9f89157a6c1563bb2645d24
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-10-19 05:59:48 +0000

    [Add]ゲストログイン機能

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,6 +4,21 @@ class Users::SessionsController < Devise::SessionsController
   before_action :reject_inactive_user, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    flash[:notice] = 'ゲストユーザでログインしました'
+    redirect_to user_path(user)
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
