b0559d3e839eff9847aceb2c28f2aef03023777f
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-10-22 05:43:18 +0000

    [Add]管理者側サービス管理機能

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :show, :edit, :update]
     resources :reviews, only: [:index, :show, :destroy]
     resources :tags, only: [:index, :destroy]
+    resources :items, only: [:index, :destroy]
 
   end
 
